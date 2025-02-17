467a9b0a10e77b48e272b84ecf847e1cdd24d2ee
Haykal Makhmud <haikal252005@gmail.com>
2025-02-17 17:26:10 +0800

    feat: add server checking endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,31 @@ import bodyParser from "body-parser";
 import router from "./routes/api";
 import db from "./utils/database";
 
-async function init(){
-    try {
-        const result = await db();
-        console.log('Database status: ', result);
+async function init() {
+  try {
+    const result = await db();
+    console.log("Database status: ", result);
 
-        const app = express();
-        app.use(bodyParser.json());
-        
-        const PORT = 3000;
-        
+    const app = express();
+    app.use(bodyParser.json());
 
-        app.use('/api', router);
-        
-        app.listen(PORT, ()=> {
-            console.log(`Server running on http://localhost:${PORT}`);
-        })
-    } catch (error) {
-        console.log(error);
-    }
+    const PORT = 3000;
+
+    app.get("/", (req, res) => {
+      res.status(200).json({
+        message: "Server is running",
+        data: null,
+      });
+    });
+
+    app.use("/api", router);
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-
 
 init();
